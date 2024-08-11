cfeb09ad87fff5abc1f9ab02121a5b6d09b51fb0
TheNetherPug <91162221+TheNetherPug@users.noreply.github.com>
2024-08-11 12:58:14 -0400

    Dynamic music + volume changes on computers

--- game/level/audio_stream_player.gd
+++ game/level/audio_stream_player.gd
@@ -6,8 +6,27 @@ func _ready() -> void:
 	if not logic : logic = OrbitalDecayLogic.first()
 
 
-func _process(_delta: float) -> void:
+func _on_timer_timeout() -> void:
 	if not logic : logic = OrbitalDecayLogic.first()
 	if not logic: return
+	
+	if logic.percentage > 0.5:
+		unmute(0)
+		return
+	if logic.percentage > 0.4:
+		unmute(1)
+		return
+	if logic.percentage > 0.35:
+		unmute(2)
+		return
+	if logic.percentage > 0.25:
+		unmute(3)
+		return
+	unmute(4)
 
-	print(logic.percentage)
+
+func unmute(index: int) -> void:
+	for i in stream.stream_count:
+		if i == index: continue
+		stream.set_sync_stream_volume(i, -60.0)
+	stream.set_sync_stream_volume(index, 0.0)
