8c5b484ce09eba6b8aef7404fd35af00629e5654
TheNetherPug <91162221+TheNetherPug@users.noreply.github.com>
2024-08-11 16:52:36 -0400

    Repair particle

--- game/player/fixes_problems.gd
+++ game/player/fixes_problems.gd
@@ -6,5 +6,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			if HighlightsInteractions.current_highlight.has_meta('has_problems'):
 				HighlightsInteractions.current_highlight.remove_meta('has_problems')
 				Audio.play_named_sfx(NamedAudio.SFX.SFX_FixThing, false)
+				print(HighlightsInteractions.current_highlight)
+				HighlightsInteractions.current_highlight.get_node("FixParticle").emitting = true
 				for sprite in get_tree().get_nodes_in_group('tutorial_sprites'):
 					sprite.modulate.a = 0.0
